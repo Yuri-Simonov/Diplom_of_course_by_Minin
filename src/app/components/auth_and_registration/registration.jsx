c972a37f3c8fd1f8357219ff45b954fd6c2d2659
Yuri Simonov <y.simonov@alef.im>
2021-11-07 13:47:58 +0300

    сделана валидация регистрации и поиск товаров через инпут поиска

--- src/app/components/auth_and_registration/registration.jsx
+++ src/app/components/auth_and_registration/registration.jsx
@@ -1,35 +1,111 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
 import { Link } from "react-router-dom";
+import { validator } from "../../../utils/validator";
+import Inputs from "./inputs";
 
 const Registration = () => {
+    const [data, setData] = useState({ email: "", password: "" });
+    const [errors, setErrors] = useState();
+    const handleChange = ({ target }) => {
+        setData((prevState) => ({
+            ...prevState,
+            [target.name]: target.value
+        }));
+    };
+
+    const validatorConfig = {
+        email: {
+            isRequired: {
+                message: "Электронная почта обязательна для заполнения"
+            },
+            isEmail: {
+                message: "Электронная почта введена некорректно"
+            }
+        },
+        password: {
+            isRequired: {
+                message: "Пароль обязателен для заполнения"
+            },
+            isCapitalSymbol: {
+                message:
+                    "Пароль должен содержать как минимум одну заглавную букву"
+            },
+            isContainDigit: {
+                message: "Пароль должен содержать как минимум одно число"
+            },
+            minValue: {
+                message: "Пароль должен содержать как минимум 8 знаков",
+                value: 8
+            }
+        }
+    };
+
+    let isValidValue = false;
+    if (errors && Object.keys(errors).length === 0) {
+        isValidValue = true;
+    }
+    useEffect(() => {
+        validate();
+    }, [data]);
+    /* eslint no-unreachable-loop: ["error", { "ignore": ["ForInStatement", "ForOfStatement"] }] */
+    const validate = () => {
+        const errors = validator(data, validatorConfig);
+        setErrors(errors);
+        return Object.keys(errors).length === 0;
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const isValid = validate();
+        if (!isValid) return;
+        console.log(data);
+    };
+
+    const getSubmittClasses = () => {
+        return (
+            "authorization__submit" +
+            (!isValidValue ? " authorization__submit" + "-not-active" : "")
+        );
+    };
+
     return (
         <main className="shop authorization">
             <div className="container">
                 <section className="authorization__body">
                     <h2 className="authorization__title title">Регистрация</h2>
-                    <form action="#" className="authorization__form">
-                        <input
-                            type="text"
-                            className="authorization__login"
-                            placeholder="Логин"
+                    <form
+                        action="#"
+                        className="authorization__form"
+                        onSubmit={handleSubmit}
+                    >
+                        <Inputs
+                            name="email"
+                            value={data.email}
+                            onChange={handleChange}
+                            error={errors}
+                            messagePlaceholder="Email"
+                            classNameInput="authorization__email"
                         />
-                        <input
+                        <Inputs
                             type="password"
-                            className="authorization__password"
-                            placeholder="Пароль"
+                            name="password"
+                            value={data.password}
+                            onChange={handleChange}
+                            error={errors}
+                            messagePlaceholder="Password"
+                            classNameInput="authorization__password"
                         />
-                        <input
-                            type="submit"
-                            className="authorization__submit"
-                            value="Зарегистрироваться"
-                        />
+                        <button className={getSubmittClasses()}>
+                            Зарегистрироваться
+                        </button>
                     </form>
-					<Link
-                            to="/authorization"
-                            className="authorization__registration hover"
-                        >
-                            Войти
-                        </Link>
+                    <Link
+                        to="/authorization"
+                        className="authorization__registration hover"
+                    >
+                        Войти
+                    </Link>
                 </section>
             </div>
         </main>
